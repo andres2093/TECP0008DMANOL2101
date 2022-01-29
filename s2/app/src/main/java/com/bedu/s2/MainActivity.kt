ffcb57bf8e3d35fbd4ce1bece53e53ac643e84ca
Andres Ramirez <andres200493@gmail.com>
2022-01-29 13:16:50 -0600

    S2

---
+++ s2/app/src/main/java/com/bedu/s2/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.bedu.s2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnAccept: Button
+    private lateinit var txvMsg: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+
+        btnAccept = findViewById(R.id.btnAccept)
+        txvMsg = findViewById(R.id.txvMsg)
+
+        btnAccept.setOnClickListener {
+            txvMsg.text = getString(R.string.btn_clicked)
+        }
+    }
+}

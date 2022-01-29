f9a06d300e709e0197ffebf0bf542b97c5a9962b
Andres Ramirez <andres200493@gmail.com>
2022-01-29 14:45:02 -0600

    S2 -> S3

--- s2/app/src/main/java/com/bedu/s2/ThirdActivity.kt
+++ s2/app/src/main/java/com/bedu/s2/ThirdActivity.kt
@@ -5,6 +5,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import coil.load
 
 class ThirdActivity : AppCompatActivity() {
 
@@ -26,6 +27,8 @@ class ThirdActivity : AppCompatActivity() {
         editPhone = findViewById(R.id.edtPhone)
         editEmail = findViewById(R.id.edtEmail)
         btnAccept = findViewById(R.id.btnAccept)
+
+        imvStatus.load("https://raw.githubusercontent.com/beduExpert/Kotlin-Intermedio-2020/master/images/android-kotlin.png")
 
         btnAccept.setOnClickListener {
             if (

ea3749082d9d62da40ba32e6726d0e4db593ea99
Andres Ramirez <andres200493@gmail.com>
2022-02-19 10:59:46 -0600

    S7 -> R2, E3

--- s72/app/src/main/java/org/bedu/s7/MainActivity.kt
+++ s72/app/src/main/java/org/bedu/s7/MainActivity.kt
@@ -1,17 +1,17 @@
 package org.bedu.s7
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.ContextMenu
-import android.view.Menu
-import android.view.MenuItem
-import android.view.View
+import android.view.*
 import android.widget.Button
 import android.widget.PopupMenu
 import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private var actionMode: ActionMode? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,6 +40,14 @@ class MainActivity : AppCompatActivity() {
             }
             popupMenu.show()
         }
+        findViewById<Button>(R.id.button).setOnLongClickListener {
+            if (actionMode == null) actionMode = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                startActionMode(ActionModeCallback(), ActionMode.TYPE_FLOATING)
+            } else {
+                startActionMode(ActionModeCallback())
+            }
+            true
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -52,10 +60,18 @@ class MainActivity : AppCompatActivity() {
         when(item.itemId){
             R.id.help -> {
                 Toast.makeText(this, "Help", Toast.LENGTH_SHORT).show()
+                if (item.isChecked) {
+                    Toast.makeText(this, "True", Toast.LENGTH_SHORT).show()
+                    item.isChecked = false
+                } else {
+                    Toast.makeText(this, "False", Toast.LENGTH_SHORT).show()
+                    item.isChecked = true
+                }
                 return true
             }
             R.id.search -> {
                 Toast.makeText(this, "Search", Toast.LENGTH_SHORT).show()
+                item.isChecked = !item.isChecked
                 return true
             }
             R.id.sub_menu -> {
@@ -78,5 +94,42 @@ class MainActivity : AppCompatActivity() {
         super.onCreateContextMenu(menu, v, menuInfo)
         val inflater = menuInflater
         inflater.inflate(R.menu.context_menu, menu)
+    }
+
+    inner class ActionModeCallback : ActionMode.Callback {
+
+        override fun onCreateActionMode(mode: ActionMode?, menu: Menu?): Boolean {
+            val inflater = mode?.menuInflater
+            inflater?.inflate(R.menu.action_menu, menu)
+            mode?.title = "Action menu"
+            return true
+        }
+
+        override fun onPrepareActionMode(mode: ActionMode?, menu: Menu?): Boolean {
+            menu?.findItem(R.id.cast)?.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS)
+            return true
+        }
+
+        override fun onActionItemClicked(mode: ActionMode?, item: MenuItem?): Boolean {
+            when (item?.itemId){
+                R.id.add -> {
+                    actionMode?.title = item.title
+                    Toast.makeText(applicationContext, item.title, Toast.LENGTH_SHORT).show()
+                    actionMode?.finish()
+                    return true
+                }
+                R.id.cast -> {
+                    actionMode?.title = item.title
+                    Toast.makeText(applicationContext, item.title, Toast.LENGTH_SHORT).show()
+                    actionMode?.finish()
+                    return true
+                }
+            }
+            return false
+        }
+
+        override fun onDestroyActionMode(mode: ActionMode?) {
+            actionMode = null
+        }
     }
 }

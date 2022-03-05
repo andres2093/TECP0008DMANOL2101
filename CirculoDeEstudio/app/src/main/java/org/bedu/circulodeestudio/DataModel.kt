665ad70042f5015c83073d2e7cee7ab7e5e54df5
Andres Ramirez <andres200493@gmail.com>
2022-03-05 11:31:31 -0600

    Circulo de estudio

---
+++ CirculoDeEstudio/app/src/main/java/org/bedu/circulodeestudio/DataModel.kt
@@ -0,0 +1,27 @@
+package org.bedu.circulodeestudio
+
+import androidx.annotation.ColorInt
+
+
+sealed class DataModel {
+    data class Header(
+        @ColorInt val bgColor: Int,
+        val title: String
+    ) : DataModel()
+
+    data class Family(
+        val name: String,
+        val relationship: String
+    ) : DataModel()
+
+    data class Friend(
+        val name: String,
+        val gender: String
+    ) : DataModel()
+
+    data class Colleague(
+        val name: String,
+        val organization: String,
+        val designation: String
+    ) : DataModel()
+}
